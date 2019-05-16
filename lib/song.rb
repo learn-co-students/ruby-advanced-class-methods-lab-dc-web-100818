9e8d751433a59e09ca8d6e5904f12235fd66d2ca
jennyjean <jennyjean8675309@yahoo.com>
2019-05-16 16:03:44 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,6 @@
+require 'rb-readline'
+require 'pry'
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +13,55 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = self.new
+    new_song.save
+    new_song
+  end
+
+  def self.new_by_name(name)
+    new_song = self.new
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    new_song = self.new_by_name(name)
+    new_song.save
+    new_song
+  end
+
+  def self.find_by_name(name)
+    self.all.find { |song| song.name == name }
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song| song.name.downcase }
+  end
+
+  # "Taylor Swift - Blank Space.mp3"
+  def self.new_from_filename(filename)
+    artist_name = filename.split(' - ')[0]
+    name = filename.split(' - ')[1].split('.')[0]
+
+    new_song = self.new
+    new_song.artist_name = artist_name
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_from_filename(filename)
+    new_song = self.new_from_filename(filename)
+    new_song.save
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
+
+Song.new_from_filename("Thundercat - For Love I Come.mp3")
